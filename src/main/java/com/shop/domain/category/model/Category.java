0fcc00844a50f68bca14d51e804a98da2309ab86
yeilkk <yeil99@naver.com>
2025-11-24 15:12:51 +0900

    :sparkles:[KTS-37] 상품 및 카테고리 도메인 연관 관계

---
+++ src/main/java/com/shop/domain/category/model/Category.java
@@ -0,0 +1,45 @@
+package com.shop.domain.category.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shop.domain.product.model.Product;
+import com.shop.global.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Category extends BaseEntity {
+
+	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent_category_id")
+	private Category parent;
+
+	@OneToMany(mappedBy = "parent")
+	private List<Category> children = new ArrayList<>();
+
+	@OneToMany(mappedBy = "category")
+	private List<Product> products = new ArrayList<>();
+
+}

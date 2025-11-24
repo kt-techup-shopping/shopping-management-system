fd03622d369702d9f1481175a63b8db50c8236f3
yeilkk <yeil99@naver.com>
2025-11-25 01:59:05 +0900

    :recycle:[KTS-37] category 도메인 수정

--- src/main/java/com/shop/domain/category/model/Category.java
+++ src/main/java/com/shop/domain/category/model/Category.java
@@ -6,28 +6,17 @@ import java.util.List;
 import com.shop.domain.product.model.Product;
 import com.shop.global.common.BaseEntity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "category")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor
 public class Category extends BaseEntity {
 
 	private String name;

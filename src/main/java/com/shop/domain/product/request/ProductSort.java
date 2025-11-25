840e516359645a37cf9c0f3ccdd23711627c2b03
yeilkk <yeil99@naver.com>
2025-11-26 01:16:36 +0900

    :recycle: [KTS-54] 정렬 처리 방법 주석

--- src/main/java/com/shop/domain/product/request/ProductSort.java
+++ src/main/java/com/shop/domain/product/request/ProductSort.java
@@ -8,10 +8,10 @@ import com.shop.global.common.CustomException;
 import com.shop.global.common.ErrorCode;
 
 public enum ProductSort {
-	LATEST("latest"),
-	PRICE_ASC("price_asc"),
-	PRICE_DESC("price_desc"),
-	DEFAULT("default");
+	LATEST("latest"),			// 최신순
+	PRICE_ASC("price_asc"),		// 가격 낮은순
+	PRICE_DESC("price_desc"),	// 가격 높은순
+	DEFAULT("default");			// 기본값 (ID)
 
 	private final String sort;
 
@@ -19,19 +19,22 @@ public enum ProductSort {
 		this.sort = sort;
 	}
 
+	// 요청값과 enum 값이 일치하는지 확인
 	public boolean matches(String s) {
 		return sort.equalsIgnoreCase(s);
 	}
 
 	public static ProductSort from(String sort) {
+		// 정렬 옵션이 없으면 기본값 반환
 		if (Strings.isBlank(sort)) {
 			return DEFAULT;
 		}
 
+		// 요청값과 일치하는 enum 반환
 		return Arrays.stream(values())
 			.filter(v -> v.matches(sort))
 			.findFirst()
-			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_SORT_OPTION));
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_SORT_OPTION)); // 유효하지 않은 정렬 요청 시 예외 처리
 	}
 }
 

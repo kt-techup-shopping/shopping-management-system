5b4e0106aedfc01320962ef44bc234056fdb887f
gichan222 <repitor15@gmail.com>
2025-11-27 16:33:07 +0900

    :sparkles: [KTS-49] 조회 경로 추가

--- src/main/java/com/shop/domain/review/controller/ReviewController.java
+++ src/main/java/com/shop/domain/review/controller/ReviewController.java
@@ -1,22 +1,32 @@
 package com.shop.domain.review.controller;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shop.domain.review.request.ReviewCreateRequest;
 import com.shop.domain.review.request.ReviewLikeRequest;
 import com.shop.domain.review.request.ReviewUpdateRequest;
+import com.shop.domain.review.response.ReviewPageResponse;
+import com.shop.domain.review.response.ReviewResponse;
 import com.shop.domain.review.service.ReviewService;
+import com.shop.domain.user.model.User;
 import com.shop.global.common.ApiResult;
+import com.shop.global.common.Paging;
 import com.shop.global.security.DefaultCurrentUser;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -78,4 +88,49 @@ public class ReviewController {
 		reviewService.updateReviewLike(reviewLikeRequest, reviewId, defaultCurrentUser.getId());
 		return ApiResult.ok();
 	}
+
+	@GetMapping("")
+	@ResponseStatus(HttpStatus.OK)
+	public ApiResult<Page<ReviewPageResponse>> getReviews(
+		@RequestParam Long productId,
+		@RequestParam(required = false, defaultValue = "oldest") String sort,
+		@Parameter Paging paging,
+		@AuthenticationPrincipal DefaultCurrentUser defaultCurrentUser
+	) {
+		Long userId = Optional.ofNullable(defaultCurrentUser)
+			.map(DefaultCurrentUser::getId)
+			.orElse(null);
+
+		Page<ReviewPageResponse> reviewPage = reviewService.getReviewPage(userId, productId, sort, paging.toPageable());
+
+		return ApiResult.ok(reviewPage);
+	}
+
+
+	// 특정 사용자 리뷰 조회
+	@GetMapping("/user")
+	public ApiResult<Page<ReviewPageResponse>> getUserReviews(
+		@RequestParam String userUUID,
+		@AuthenticationPrincipal DefaultCurrentUser defaultCurrentUser,
+		@Parameter Paging paging
+	) {
+		Long userId = Optional.ofNullable(defaultCurrentUser)
+			.map(DefaultCurrentUser::getId)
+			.orElse(null);
+		return ApiResult.ok(reviewService.getUserReviewsByUuid(userUUID, userId, paging.toPageable()));
+	}
+
+	// 단일 리뷰 조회
+	@GetMapping("/single")
+	public ApiResult<ReviewResponse> getReview(
+		@RequestParam Long reviewId,
+		@AuthenticationPrincipal DefaultCurrentUser defaultCurrentUser
+	) {
+		Long userId = Optional.ofNullable(defaultCurrentUser)
+			.map(DefaultCurrentUser::getId)
+			.orElse(null);
+		return ApiResult.ok(reviewService.getReview(reviewId, userId));
+	}
+
+
 }

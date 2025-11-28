9a460da68c45736a3bfad819a65fa03f31fc2774
yeilkk <yeil99@naver.com>
2025-11-29 04:04:22 +0900

    :recycle: [KTS-57] LocalDateTime 제외

--- src/main/java/com/shop/domain/product/model/Product.java
+++ src/main/java/com/shop/domain/product/model/Product.java
@@ -63,8 +63,6 @@ public class Product extends BaseEntity {
 		this.description = description;
 		this.color = color;
 		this.category = category;
-		this.createdAt = LocalDateTime.now();
-		this.updatedAt = LocalDateTime.now();
 	}
 
 	public Product(String name, Long price, Long stock) {
@@ -99,7 +97,6 @@ public class Product extends BaseEntity {
 		this.stock += deltaStock;
 		this.status = status;
 		this.category = category;
-		this.updatedAt = LocalDateTime.now();
 	}
 
 	public void soldOut() {

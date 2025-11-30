3812687daffb228507cbe1447a7979ff328be376
yeilkk <yeil99@naver.com>
2025-11-30 19:51:37 +0900

    :sparkles: [KTS-19] 관리자 주문 상세 조회 controller

--- src/main/java/com/shop/domain/order/controller/AdminOrderController.java
+++ src/main/java/com/shop/domain/order/controller/AdminOrderController.java
@@ -2,22 +2,27 @@ package com.shop.domain.order.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shop.domain.order.repository.OrderRepositoryCustom;
 import com.shop.domain.order.response.AdminOrderDetailResponse;
+import com.shop.domain.order.response.AdminOrderDetailUserResponse;
 import com.shop.domain.order.service.AdminOrderService;
 import com.shop.global.common.ApiResult;
 import com.shop.global.common.Paging;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/admin/orders")
 @RequiredArgsConstructor
+@RequestMapping("/admin/orders")
+@Tag(name = "관리자 주문 API")
 public class AdminOrderController {
 	private final OrderRepositoryCustom orderRepository;
 	private final AdminOrderService adminOrderService;
@@ -33,8 +38,8 @@ public class AdminOrderController {
 	// 	return ApiResult.ok(orderRepository.search(keyword, paging.toPageable()));
 	// }
 
-	// 관리자 주문 목록 조회
 	@GetMapping
+	@Operation(summary = "관리자 주문 목록 조회", description = "관리자가 주문 ID, 사용자 ID, 주문 상태를 조건으로 주문 목록을 조회합니다.")
 	public ApiResult<Page<AdminOrderDetailResponse>> getOrders(
 		@RequestParam(required = false) Long orderId,
 		@RequestParam(required = false) Long userId,
@@ -42,5 +47,11 @@ public class AdminOrderController {
 		@Parameter Paging paging
 	) {
 		return ApiResult.ok(adminOrderService.getOrders(orderId, userId, status, paging));
+	}
+
+	@GetMapping("/{id}/detail")
+	@Operation(summary = "관리자 주문 상세 조회", description = "관리자가 특정 주문 ID의 상세 정보를 조회합니다.")
+	public ApiResult<AdminOrderDetailUserResponse> getAdminOrderDetailById(@PathVariable Long id) {
+		return ApiResult.ok(adminOrderService.getAdminOrderDetailById(id));
 	}
 }

ce20054e7a0d312217f20e7a745b088757bdb049
ejinn1 <ejinn1@naver.com>
2025-12-03 10:24:59 +0900

    :sparkles: [KTS-96] 관리자 생성 테스트

--- src/test/java/com/shop/domain/user/model/UserTest.java
+++ src/test/java/com/shop/domain/user/model/UserTest.java
@@ -1,7 +1,5 @@
 package com.shop.domain.user.model;
 
-
-
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -14,6 +12,7 @@ class UserTest {
 
 	@Test
 	void 일반_유저_생성() {
+
 		var user = User.normalUser(
 			"testUser",
 			UUID.randomUUID(),
@@ -28,5 +27,23 @@ class UserTest {
 		assertThat(user.getRole()).isEqualTo(Role.USER);
 		assertThat(user.getStatus()).isEqualTo(Status.ACTIVE);
 		assertThat(user.getIsDeleted()).isFalse();
+	}
+
+	@Test
+	void 관리자_생성() {
+		var admin = User.admin(
+			"adminUser",
+			UUID.randomUUID(),
+			"password",
+			"test",
+			"test@test.com",
+			"010-0000-0000",
+			Gender.MALE,
+			LocalDate.now()
+		);
+
+		assertThat(admin.getRole()).isEqualTo(Role.ADMIN);
+		assertThat(admin.getStatus()).isEqualTo(Status.ACTIVE);
+		assertThat(admin.getIsDeleted()).isFalse();
 	}
 }

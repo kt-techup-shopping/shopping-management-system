920a8ddcd006fc3acf4dffba670a2b42d1601547
AkiStory <sonsungjeon@gmail.com>
2025-12-05 10:21:26 +0900

    :sparkles: [KTS-108] 배송 PENDING상태로 변경

--- src/test/java/com/shop/domain/delivery/model/DeliveryTest.java
+++ src/test/java/com/shop/domain/delivery/model/DeliveryTest.java
@@ -18,4 +18,16 @@ class DeliveryTest {
 		assertThat(delivery.getWaybillNo()).isNull();
 	}
 
+	@Test
+	void 배송_상태_PENDING으로_변경() {
+		var order = new Order();
+		var delivery = new Delivery(order);
+		delivery.updateReady("CJ-123-456-7890"); // 먼저 READY 상태로 변경
+
+		delivery.updatePending();
+
+		assertThat(delivery.getDeliveryStatus()).isEqualTo(DeliveryStatus.PENDING);
+		assertThat(delivery.getWaybillNo()).isNull();
+	}
+
 }

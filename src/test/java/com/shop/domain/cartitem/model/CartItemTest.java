79914af9348edba2585c98e3e839ad4b66fe19af
AkiStory <sonsungjeon@gmail.com>
2025-12-05 09:59:34 +0900

    :sparkles: [KTS-95] 장바구니 수량 추가 성공 실패

--- src/test/java/com/shop/domain/cartitem/model/CartItemTest.java
+++ src/test/java/com/shop/domain/cartitem/model/CartItemTest.java
@@ -7,8 +7,6 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import com.shop.domain.cart.model.Cart;
 import com.shop.domain.product.model.Product;
@@ -22,11 +20,11 @@ class CartItemTest {
 
 	@Test
 	void 카트아이템_생성_성공() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234", "QWER",
-			"test@example.com", "010-1234-4567", Gender.MALE,
-			LocalDate.now(), Role.USER, Status.ACTIVE);
+		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
+			"QWER", "test@example.com", "010-1234-4567",
+			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
 		var cart = new Cart(user);
-		var product = new Product("테스트상품", 10000L, 10L);
+		var product = new Product("테스트상품", 10000L, 100L);
 
 		var cartItem = new CartItem(5L, cart, product);
 
@@ -35,17 +33,72 @@ class CartItemTest {
 		assertThat(cartItem.getProduct()).isEqualTo(product);
 	}
 
-	@ParameterizedTest
-	@ValueSource(longs = {0L, -1L, -10L})
-	void 카트아이템_생성_실패_수량이_0_이하(Long quantity) {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234", "QWER",
-			"test@example.com", "010-1234-4567", Gender.MALE,
-			LocalDate.now(), Role.USER, Status.ACTIVE);
+	@Test
+	void 카트아이템_생성_실패_수량이_0_이하() {
+		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
+			"QWER", "test@example.com", "010-1234-4567",
+			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
 		var cart = new Cart(user);
-		var product = new Product("테스트상품", 10000L, 10L);
+		var product = new Product("테스트상품", 10000L, 100L);
 
-		assertThrowsExactly(CustomException.class, () ->
-			new CartItem(quantity, cart, product)
+		assertThrowsExactly(CustomException.class,
+			() -> new CartItem(-1L, cart, product)
+		);
+	}
+
+	@Test
+	void 카트아이템_수량_업데이트_성공() {
+		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
+			"QWER", "test@example.com", "010-1234-4567",
+			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
+		var cart = new Cart(user);
+		var product = new Product("테스트상품", 10000L, 100L);
+		var cartItem = new CartItem(5L, cart, product);
+
+		cartItem.updateQuantity(10L);
+	}
+
+	@Test
+	void 카트아이템_수량_업데이트_실패_0_이하() {
+		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
+			"QWER", "test@example.com", "010-1234-4567",
+			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
+		var cart = new Cart(user);
+		var product = new Product("테스트상품", 10000L, 100L);
+		var cartItem = new CartItem(5L, cart, product);
+
+		assertThrowsExactly(CustomException.class,
+			() -> cartItem.updateQuantity(-10L)
+		);
+	}
+
+	@Test
+	void 카트아이템_수량_추가_성공() {
+		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
+			"QWER", "test@example.com", "010-1234-4567",
+			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
+		var cart = new Cart(user);
+		var product = new Product("테스트상품", 10000L, 100L);
+		var cartItem = new CartItem(5L, cart, product);
+
+		cartItem.addQuantity(3L);
+		cartItem.addQuantity(4L);
+
+		assertThat(cartItem.getQuantity()).isEqualTo(12L);
+	}
+
+	@Test
+	void 수량_추가_실패_추가_수량이_0_이하() {
+		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
+			"QWER", "test@example.com", "010-1234-4567",
+			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
+		var cart = new Cart(user);
+		var product = new Product("테스트상품", 10000L, 100L);
+		var cartItem = new CartItem(5L, cart, product);
+
+		// When & Then
+		assertThrowsExactly(CustomException.class,
+			() -> cartItem.addQuantity(0L)
 		);
 	}
 

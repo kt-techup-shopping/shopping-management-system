38f32579013dcf988107c03e6d37090ca1cf468c
AkiStory <sonsungjeon@gmail.com>
2025-12-07 20:57:40 +0900

    :recycle: [KTS-95] setUp메서드 추가

--- src/test/java/com/shop/domain/cartitem/model/CartItemTest.java
+++ src/test/java/com/shop/domain/cartitem/model/CartItemTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.shop.domain.cart.model.Cart;
@@ -18,16 +19,26 @@ import com.shop.global.common.CustomException;
 
 class CartItemTest {
 
-	@Test
-	void 카트아이템_생성_성공() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
+	private Cart cart;
+	private User user;
+
+	@BeforeEach
+	void setUp() {
+		user = new User("QWER", UUID.randomUUID(), "Qwer!234",
 			"QWER", "test@example.com", "010-1234-4567",
 			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
-		var cart = new Cart(user);
+
+		cart = new Cart(user);
+	}
+
+	@Test
+	void 카트아이템_생성_성공() {
+		//given
 		var product = new Product("테스트상품", 10000L, 100L);
 
 		var cartItem = new CartItem(5L, cart, product);
 
+		//then
 		assertThat(cartItem.getQuantity()).isEqualTo(5L);
 		assertThat(cartItem.getCart()).isEqualTo(cart);
 		assertThat(cartItem.getProduct()).isEqualTo(product);
@@ -35,12 +46,10 @@ class CartItemTest {
 
 	@Test
 	void 카트아이템_생성_실패_수량이_0_이하() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
-			"QWER", "test@example.com", "010-1234-4567",
-			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
-		var cart = new Cart(user);
+		//given
 		var product = new Product("테스트상품", 10000L, 100L);
 
+		//then
 		assertThrowsExactly(CustomException.class,
 			() -> new CartItem(-1L, cart, product)
 		);
@@ -48,25 +57,24 @@ class CartItemTest {
 
 	@Test
 	void 카트아이템_수량_업데이트_성공() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
-			"QWER", "test@example.com", "010-1234-4567",
-			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
-		var cart = new Cart(user);
+		//given
 		var product = new Product("테스트상품", 10000L, 100L);
 		var cartItem = new CartItem(5L, cart, product);
 
+		//when
 		cartItem.updateQuantity(10L);
+
+		//then
+		assertThat(cartItem.getQuantity()).isEqualTo(10L);
 	}
 
 	@Test
 	void 카트아이템_수량_업데이트_실패_0_이하() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
-			"QWER", "test@example.com", "010-1234-4567",
-			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
-		var cart = new Cart(user);
+		//given
 		var product = new Product("테스트상품", 10000L, 100L);
 		var cartItem = new CartItem(5L, cart, product);
 
+		//then
 		assertThrowsExactly(CustomException.class,
 			() -> cartItem.updateQuantity(-10L)
 		);
@@ -74,29 +82,25 @@ class CartItemTest {
 
 	@Test
 	void 카트아이템_수량_추가_성공() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
-			"QWER", "test@example.com", "010-1234-4567",
-			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
-		var cart = new Cart(user);
+		//given
 		var product = new Product("테스트상품", 10000L, 100L);
 		var cartItem = new CartItem(5L, cart, product);
 
+		//when
 		cartItem.addQuantity(3L);
 		cartItem.addQuantity(4L);
 
+		//then
 		assertThat(cartItem.getQuantity()).isEqualTo(12L);
 	}
 
 	@Test
 	void 수량_추가_실패_추가_수량이_0_이하() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
-			"QWER", "test@example.com", "010-1234-4567",
-			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
-		var cart = new Cart(user);
+		//given
 		var product = new Product("테스트상품", 10000L, 100L);
 		var cartItem = new CartItem(5L, cart, product);
 
-		// When & Then
+		//then
 		assertThrowsExactly(CustomException.class,
 			() -> cartItem.addQuantity(0L)
 		);
@@ -104,17 +108,14 @@ class CartItemTest {
 
 	@Test
 	void 전체_가격_계산() {
-		var user = new User("QWER", UUID.randomUUID(), "Qwer!234",
-			"QWER", "test@example.com", "010-1234-4567",
-			Gender.MALE, LocalDate.now(), Role.USER, Status.ACTIVE);
-		var cart = new Cart(user);
+		//given
 		var product = new Product("테스트상품", 10000L, 100L);
 		var cartItem = new CartItem(5L, cart, product);
 
-		// When
+		//when
 		Long totalPrice = cartItem.getTotalPrice();
 
-		// Then
+		//then
 		assertThat(totalPrice).isEqualTo(50000L);
 	}
 

18b3de769d280bb416f04b3f1b4c537831b398a8
ejinn1 <ejinn1@naver.com>
2025-12-16 20:31:25 +0900

    :fire: [KTS-127] 사용하지 않는 코드 삭제

--- auth/src/main/java/com/shop/controller/AuthController.java
+++ auth/src/main/java/com/shop/controller/AuthController.java
@@ -26,7 +26,6 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/auth")
 public class AuthController {
 	private final AuthService authService;
-	// private final UserService userService;
 
 	@Operation(summary = "로그인", description = "아이디와 비밀번호로 로그인합니다.")
 	@ApiErrorCodeExamples({
@@ -40,15 +39,6 @@ public class AuthController {
 		return ApiResult.ok(new LoginResponse(pair.getFirst(), pair.getSecond()));
 	}
 
-	// @Operation(summary = "회원가입", description = "새로운 사용자를 등록합니다.")
-	// @ApiErrorCodeExample(ErrorCode.EXIST_USER)
-	// @PostMapping("/signup")
-	// @ResponseStatus(HttpStatus.CREATED)
-	// public ApiResult<UserCreateResponse> signup(@RequestBody @Valid UserCreateRequest request) {
-	// 	var user = userService.create(request);
-	// 	return ApiResult.ok(UserCreateResponse.from(user));
-	// }
-
 	@Operation(summary = "토큰 재발급", description = "Refresh Token을 이용해 Access Token을 재발급합니다.")
 	@ApiErrorCodeExamples({
 		ErrorCode.INVALID_REFRESH_TOKEN,
@@ -60,28 +50,4 @@ public class AuthController {
 		var pair = authService.refresh(request.refreshToken());
 		return ApiResult.ok(new LoginResponse(pair.getFirst(), pair.getSecond()));
 	}
-
-	// @Operation(summary = "로그아웃", description = "현재 로그인된 사용자를 로그아웃합니다.")
-	// @PostMapping("/logout")
-	// @ResponseStatus(HttpStatus.OK)
-	// public ApiResult<Void> postLogout(@AuthenticationPrincipal CurrentUser currentUser) {
-	// 	authService.logout(currentUser.getId());
-	// 	return ApiResult.ok();
-	// }
-
-	// @Operation(summary = "비밀번호 변경", description = "현재 비밀번호를 확인하고 새 비밀번호로 변경합니다.")
-	// @ApiErrorCodeExamples({
-	// 	ErrorCode.NOT_FOUND_USER,
-	// 	ErrorCode.DOES_NOT_MATCH_OLD_PASSWORD,
-	// 	ErrorCode.CAN_NOT_ALLOWED_SAME_PASSWORD,
-	// })
-	// @PostMapping("/reset-password/confirm")
-	// @ResponseStatus(HttpStatus.OK)
-	// public ApiResult<Void> postResetPasswordConfirm(
-	// 	@AuthenticationPrincipal CurrentUser currentUser,
-	// 	@RequestBody @Valid UserUpdatePasswordRequest request
-	// ) {
-	// 	userService.changePassword(currentUser.getId(), request.oldPassword(), request.newPassword());
-	// 	return ApiResult.ok();
-	// }
 }

eb61b5d7c07157c9466fd0ddc8d5748dc0c58eef
yeilkk <yeil99@naver.com>
2025-12-17 16:46:50 +0900

    :bug: [KTS-133] 빌드 kotlin 제외

--- admin/src/main/java/com/shop/product/request/AdminProductSoldOutRequest.java
+++ admin/src/main/java/com/shop/product/request/AdminProductSoldOutRequest.java
@@ -2,8 +2,7 @@ package com.shop.product.request;
 
 import java.util.List;
 
-import org.jetbrains.annotations.NotNull;
-
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotEmpty;
 
 public record AdminProductSoldOutRequest(

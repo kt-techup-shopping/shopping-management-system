a85c1fdd5a513ab795af5477aad109228caa7c1a
ejinn1 <ejinn1@naver.com>
2025-12-20 10:39:35 +0900

    :sparkles: [KTS-145] 결제 성공 페이지로 리다이렉트 추가

--- user/src/main/java/com/shop/payment/controller/PaymentController.java
+++ user/src/main/java/com/shop/payment/controller/PaymentController.java
@@ -1,16 +1,23 @@
 package com.shop.payment.controller;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shop.ApiResult;
 import com.shop.ErrorCode;
 import com.shop.docs.ApiErrorCodeExamples;
+import com.shop.payment.request.PaymentConfirmRequest;
 import com.shop.payment.response.PaymentInfoResponse;
 import com.shop.payment.service.PaymentService;
 
@@ -31,6 +38,48 @@ public class PaymentController {
 	public ApiResult<PaymentInfoResponse> getPaymentPageInfo(@PathVariable Long paymentId) {
 		var paymentInfo = paymentService.getPaymentInfo(paymentId);
 		return ApiResult.ok(paymentInfo);
+	}
+
+	@GetMapping("/toss/success")
+	public String tossSuccess(
+		@RequestParam Long paymentId,
+		@RequestParam String orderId,
+		@RequestParam String paymentKey,
+		@RequestParam Long amount
+	) {
+		System.out.println(paymentId);
+		System.out.println(orderId);
+		System.out.println(paymentKey);
+		System.out.println(amount);
+
+		// paymentService.confirmAndComplete(
+		// 	paymentId,
+		// 	orderId,
+		// 	paymentKey,
+		// 	amount
+		// );
+
+		// 정적 성공 페이지로 이동
+		return "redirect:/payment-success.html"
+			+ "?paymentId=" + paymentId
+			+ "&orderId=" + URLEncoder.encode(orderId, StandardCharsets.UTF_8)
+			+ "&paymentKey=" + URLEncoder.encode(paymentKey, StandardCharsets.UTF_8)
+			+ "&amount=" + amount;
+	};
+
+	@PostMapping("/{paymentId}/confirm")
+	public void tossConfirm(
+		@PathVariable Long paymentId,
+		@RequestBody PaymentConfirmRequest request
+	) {
+		// TODO: 토스한테 confirm요청 보내기
+		paymentService.confirm(
+			paymentId,
+			request.orderId(),
+			request.paymentKey(),
+			request.amount()
+		);
+
 	}
 
 	// 결제 완료 처리
--- user/src/main/java/com/shop/payment/service/PaymentService.java
+++ user/src/main/java/com/shop/payment/service/PaymentService.java
@@ -13,6 +13,7 @@ import com.shop.payment.response.PaymentInfoResponse;
 import com.shop.payment.response.PaymentResponse;
 import com.shop.repository.order.OrderRepository;
 import com.shop.repository.payment.PaymentRepository;
+// import com.shop.toss.TossPaymentsClient;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class PaymentService {
 	private final OrderRepository orderRepository;
 	private final PaymentRepository paymentRepository;
+	// private final TossPaymentsClient tossPaymentsClient;
 
 	public void createPayment(Long orderId, PaymentType type) {
 		var order = orderRepository.findByIdOrThrow(orderId, ErrorCode.NOT_FOUND_ORDER);
@@ -92,5 +94,32 @@ public class PaymentService {
 
 		payment.cancel();
 		order.resetToPending();
+	}
+
+	public void confirm(Long paymentId, String orderId, String paymentKey, Long amount) {
+		var payment = paymentRepository.findByIdOrThrow(paymentId, ErrorCode.NOT_FOUND_PAYMENT);
+		var order = payment.getOrder();
+
+		System.out.println("결제 확인 요청 메서드까지 옴");
+
+		// 결제 상태 검증
+		Preconditions.validate(payment.isPending(), ErrorCode.INVALID_PAYMENT_STATUS);
+
+		// orderId 검증
+		// Preconditions.validate(order.getId());
+
+		// 결제 금액 검증
+		Preconditions.validate(payment
+			.getFinalAmount()
+			.equals(amount), ErrorCode.INVALID_PAYMENT_AMOUNT);
+
+		// var toss = tossPaymentsClient.confirm(paymentKey, orderId, amount);
+		//
+		// Preconditions.validate("DONE".equals(toss.status()), ErrorCode.INVALID_PAYMENT_STATUS);
+		// Preconditions.validate(amount.equals(toss.totalAmount()), ErrorCode.INVALID_PAYMENT_AMOUNT);
+		// Preconditions.validate(orderId.equals(toss.orderId()), ErrorCode.INVALID_ORDER_ID);
+		//
+		// // 내부 완료 처리
+		// System.out.println(toss);
 	}
 }

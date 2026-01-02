eecd0ebc29f8a60ad8b7d5317411a239ca5dda7b
gichan222 <repitor15@gmail.com>
2026-01-02 10:58:12 +0900

    :sparkles: [KTS-158] consumer 요청/응답 객체 사용 및 저장 로직 추가

--- chat/src/main/java/com/shop/kafka/KafkaConsumerComponent.java
+++ chat/src/main/java/com/shop/kafka/KafkaConsumerComponent.java
@@ -3,8 +3,8 @@ package com.shop.kafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
-
-import com.shop.request.ChatMessage;
+import com.shop.response.ChatResponse;
+import com.shop.service.ChatService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +14,23 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class KafkaConsumerComponent {
 
+	private static final String CHAT_SAVE_GROUP = "chat-save-group";
+
 	private final SimpMessagingTemplate messagingTemplate;
+	private final ChatService chatService;
 
 	@KafkaListener(topics = "chat-topic1", groupId = "#{ 'chat-group-' + T(java.net.InetAddress).getLocalHost().getHostName() }")
-	public void consume(ChatMessage message) {
+	public void consume(ChatResponse message) {
 		log.info("Kafka로부터 메시지 수신: {}", message);
 
 		// WebSocket 구독자들에게 메시지 전송 (/sub/chat/room/{roomId})
 		messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
 	}
 
+	// 단 하나의 서버에서만 처리하면 되는 명령어기 때문에 groupId 고정
+	@KafkaListener(topics = "chat-save", groupId = CHAT_SAVE_GROUP)
+	public void save(ChatResponse message){
+		log.info("Kafka로부터 저장 요청 수신: {}", message);
+		chatService.saveMessage(message);
+	}
 }
